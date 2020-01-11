687e30555b9d55d19fc0e713b3e2587a5ff90d62
Marcel Rico <personal.mr4@gmail.com>
2020-01-11 20:02:34 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -6,18 +6,18 @@ class Person
     @name = name
   end
 
-def get_married(person)
-    self.partner = person
-    if person.class != Person
-      begin
-        raise PartnerError
-      rescue PartnerError => error
-          puts error.message
+  def get_married(person)
+      self.partner = person
+      if person.class != Person
+        begin
+          raise PartnerError
+        rescue PartnerError => error
+            puts error.message
+        end
+      else
+        person.partner = self
       end
-    else
-      person.partner = self
-    end
-end
+  end
   
   class PartnerError < StandardError
     def message 
